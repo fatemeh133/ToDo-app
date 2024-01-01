b3c2e581ba831905979d11d8fd7f96df91b139fa
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-01-01 16:44:30 +0330

    near finish

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask() {
+  let addInput = document.querySelector(".add-input");
+
+  document.querySelector(".add-input").focus();
+  if (addInput.value !== "") {
+    let task = `<div class="Task">
+     <input type="checkbox" name="" id="task3"onclick="check(this)"/><label for="task3">
+       ${addInput.value}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/></div>`;
+
+    document.querySelector(".container").insertAdjacentHTML("beforeend", task);
+  }
+}
+
+function closeTask(closeElement) {
+  closeElement.parentElement.remove();
+}
+
+function check(checkboxElement) {
+  let label = checkboxElement.parentElement.childNodes[2];
+  label.classList.toggle("done");
+}
