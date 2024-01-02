7aede6a99e546b51a5e2e20f1623f773303e18e7
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-01-02 16:52:36 +0330

    set and get tasks to storage

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function addNewTask(addInput) {
      ${addInput.value}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/></div>`;
 
     document.querySelector(".container").insertAdjacentHTML("beforeend", task);
+    localStorage.setItem(`${addInput.value}`, `${addInput.value}`);
     addInput.value = "";
   }
 }
@@ -25,9 +26,31 @@ addInput.addEventListener("keypress", function (event) {
 
 function closeTask(closeElement) {
   closeElement.parentElement.remove();
+  //remove from storage
+  let label = closeElement.parentElement.childNodes[2];
+  localStorage.removeItem(`${label.innerHTML.trim()}`);
 }
 
 function check(checkboxElement) {
   let label = checkboxElement.parentElement.childNodes[2];
   label.classList.toggle("done");
 }
+
+window.addEventListener("load", () => {
+  for (let i = 0; i < Object.entries(localStorage).length; i++) {
+    //get all storage keys
+    let itemName = localStorage.getItem(
+      `${Object.entries(localStorage)[i][0]}`
+    );
+    //make a task base on keys
+    if (itemName !== null) {
+      let task = `<div class="Task">
+     <input type="checkbox" name="" id="task3"onclick="check(this)"/><label for="task3">
+       ${itemName}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/></div>`;
+
+      document
+        .querySelector(".container")
+        .insertAdjacentHTML("beforeend", task);
+    }
+  }
+});
