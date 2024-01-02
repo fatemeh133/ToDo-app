90ec699ffadd5d0ed5b37b5557b0a405b5b03c53
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-01-02 11:18:24 +0330

    add by enter and cleaning

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-function addTask() {
-  let addInput = document.querySelector(".add-input");
+let addInput = document.querySelector(".add-input");
 
-  document.querySelector(".add-input").focus();
+// add new task function to use multiple times
+function addNewTask(addInput) {
+  addInput.focus();
   if (addInput.value !== "") {
     let task = `<div class="Task">
-     <input type="checkbox" name="" id="task3"onclick="check(this)"/><label for="task3">
-       ${addInput.value}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/></div>`;
+   <input type="checkbox" name="" id="task3"onclick="check(this)"/><label for="task3">
+     ${addInput.value}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/></div>`;
 
     document.querySelector(".container").insertAdjacentHTML("beforeend", task);
     addInput.value = "";
   }
 }
+
+function addTaskBtn() {
+  addNewTask(addInput);
+}
+
+addInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addNewTask(addInput);
+  }
+});
 
 function closeTask(closeElement) {
   closeElement.parentElement.remove();
