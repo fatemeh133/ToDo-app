cf8b258bbedd5083f975f2a1a7c095a716f40bb9
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-01-03 16:39:30 +0330

    tried to add edit but commented

--- script.js
+++ script.js
@@ -65,12 +65,41 @@ function deleteObjectByName(nameToDelete) {
   );
   localStorage.setItem(`Tasks`, jsonString);
 }
+// function updateObjectByName(nameToUpdate, newName) {
+//   const retrievedArrayString = localStorage.getItem("Tasks");
+//   const retrievedArray = JSON.parse(retrievedArrayString);
+//   console.log(retrievedArray);
+
+//   for (let i = 0; i < retrievedArray.length; i++) {
+//     if (retrievedArray[i].taskName === nameToUpdate) {
+//       retrievedArray[i].taskName = newName;
+//       console.log(retrievedArray);
+//       // const jsonString = JSON.stringify(retrievedArray);
+//       // localStorage.setItem(`Tasks`, jsonString);
+
+//       break; // Stop the loop since we found and updated the object
+//     }
+//   }
+// }
+// updateObjectByName(`1`, `2`);
 
 function closeTask(closeElement) {
   deleteObjectByName(
     `${closeElement.parentElement.childNodes[2].innerHTML.trim()}`
   );
   closeElement.parentElement.remove();
+}
+
+function editTask(editElement) {
+  // console.log(editElement.parentElement);
+  // console.log(editElement.parentElement.childNodes[2].innerHTML.trim());
+  // let firstContent = editElement.parentElement.childNodes[2].innerHTML.trim();
+  // let taskElem = editElement.parentElement;
+  // taskElem.contentEditable = "true";
+  // editElement.parentElement.addEventListener("blur", () => {
+  //   taskElem.contentEditable = "false";
+  //   console.log(editElement.parentElement.childNodes[2]);
+  // });
 }
 
 function check(checkboxElement) {
@@ -99,7 +128,7 @@ window.addEventListener("load", () => {
     if (itemName !== null) {
       let task = `<div class="Task">
      <input type="checkbox" name="" id="task${i}"onclick="check(this)"${retrievedArray[i].checkStat}/><label for="task${i}" class="${retrievedArray[i].labelClass}">
-       ${itemName}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/></div>`;
+       ${itemName}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/> <img src="image/edit.svg" alt="" class="edit-icon" onclick="editTask(this)"/></div>`;
 
       document
         .querySelector(".container")
