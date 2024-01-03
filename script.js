ed9ff99746633ebaf7362d7944b84fd5211e3cce
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-01-03 14:17:03 +0330

    save json for order, save if checked ,click label to chkeck

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let addInput = document.querySelector(".add-input");
+let taskJson = [];
+//get tasks when reload
+const retrievedArrayString = localStorage.getItem("Tasks");
+let ConvertedJson = JSON.parse(retrievedArrayString);
+if (ConvertedJson !== null) {
+  taskJson = ConvertedJson;
+}
 
 // add new task function to use multiple times
 function addNewTask(addInput) {
@@ -9,8 +16,34 @@ function addNewTask(addInput) {
      ${addInput.value}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/></div>`;
 
     document.querySelector(".container").insertAdjacentHTML("beforeend", task);
-    localStorage.setItem(`${addInput.value}`, `${addInput.value}`);
+
+    //new
+    taskJson.push({
+      taskName: `${addInput.value}`,
+      checkStat: "",
+      labelClass: "",
+    });
+    const jsonString = JSON.stringify(taskJson);
+    localStorage.setItem(`Tasks`, jsonString);
+
     addInput.value = "";
+  }
+}
+
+function updateCheckedByName(taskName, checkStat, labelClass) {
+  const retrievedArrayString = localStorage.getItem("Tasks");
+  const retrievedArray = JSON.parse(retrievedArrayString);
+
+  for (let i = 0; i < retrievedArray.length; i++) {
+    if (retrievedArray[i].taskName === taskName) {
+      retrievedArray[i].checkStat = checkStat;
+      retrievedArray[i].labelClass = labelClass;
+
+      const jsonString = JSON.stringify(retrievedArray);
+      localStorage.setItem(`Tasks`, jsonString);
+
+      break; // Stop the loop since we found and updated the object
+    }
   }
 }
 
@@ -34,18 +67,29 @@ function closeTask(closeElement) {
 function check(checkboxElement) {
   let label = checkboxElement.parentElement.childNodes[2];
   label.classList.toggle("done");
+
+  let taskName = checkboxElement.parentElement.childNodes[2].innerHTML.trim();
+  let labelClass = checkboxElement.parentElement.childNodes[2].classList.value;
+  let checkStat = checkboxElement.parentElement.childNodes[1].checked
+    ? "checked"
+    : "";
+
+  updateCheckedByName(`${taskName}`, `${checkStat}`, `${labelClass}`);
 }
 
 window.addEventListener("load", () => {
-  for (let i = 0; i < Object.entries(localStorage).length; i++) {
+  const retrievedArrayString = localStorage.getItem("Tasks");
+  const retrievedArray = JSON.parse(retrievedArrayString);
+
+  console.log(retrievedArray);
+
+  for (let i = 0; i < retrievedArray.length; i++) {
     //get all storage keys
-    let itemName = localStorage.getItem(
-      `${Object.entries(localStorage)[i][0]}`
-    );
+    let itemName = retrievedArray[i].taskName;
     //make a task base on keys
     if (itemName !== null) {
       let task = `<div class="Task">
-     <input type="checkbox" name="" id="task3"onclick="check(this)"/><label for="task3">
+     <input type="checkbox" name="" id="task${i}"onclick="check(this)"${retrievedArray[i].checkStat}/><label for="task${i}" class="${retrievedArray[i].labelClass}">
        ${itemName}</label> <img src="image/x.svg" alt="" class="close-icon" onclick="closeTask(this)"/></div>`;
 
       document
